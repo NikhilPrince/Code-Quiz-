7c1ff248c38fbf064a0ab0d941b19c0a5f5174ba
NikhilPrince <117137916+NikhilPrince@users.noreply.github.com>
2023-04-01 17:50:25 -0700

    Placeholder questions added and question.js created

---
+++ questions.js
@@ -0,0 +1,79 @@
+var questions = [
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+
+{
+    question: "Javascript is considered what kind of language?",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the above"],
+    answer: "A. Object-oriented"
+},
+];
